a9bbe404fdc82a22f1bd165c93ad081096205a3d
Daaimah Tibrey <daaimah12392@gmail.com>
2025-03-27 09:42:29 -0700

    deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  
+  // This ensures images work with static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
