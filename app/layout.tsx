569ef1b355e26b07268144c14ac23908fe3ab2c5
Daaimah Tibrey <daaimah12392@gmail.com>
2025-03-28 10:58:48 -0700

    new svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
-import "./globals.css";
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Skills Matrix",
   description: "A tool for career exploration and skills assessment",
-};
+  icons: {
+    icon: "/career.svg",
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
